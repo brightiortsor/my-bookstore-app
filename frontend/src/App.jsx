4b082bdc01ff304de32d7f4775de159e42ff76f9
Bright Iortsor <iortsor@gmail.com>
2023-10-12 09:46:27 +0100

    Feat/setup up the frontend react project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-[#9e9e97} text-5xl text-red-800 text-center mt-8">
+        Hello World
+      </h1>
+    </div>
+  );
+};
+
+export default App;
